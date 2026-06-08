a6fb72d5b2f94a71dc27ea89caaaa37da37e16a5
Dalhat <ddalhatishaq@gmail.com>
2026-06-08 20:31:47 +0100

    Added Signup

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import { Checkout } from './pages/Checkout'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
+import { AuthProvider } from './context/AuthContext'
 
 function App() {
 
   return (
-    <div className='app'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/auth' element={<Auth />} />
-        <Route path='/checkout' element={<Checkout />}/>
-      </Routes>
-
-    </div>
+    <AuthProvider>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/auth' element={<Auth />} />
+          <Route path='/checkout' element={<Checkout />}/>
+        </Routes>
+      </div>
+    </AuthProvider>
   )
 }
 
