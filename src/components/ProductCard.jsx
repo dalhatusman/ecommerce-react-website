fa98917fd67f5206ba09e8728dc60e9a78cecc16
Dalhat <ddalhatishaq@gmail.com>
2026-06-08 14:01:54 +0100

    Fixed image alt

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 export const ProductCard = ({product}) => {
     return ( 
             <div className="product-card">
-                <img src={product.image} className="product-card-image" />
+                <img src={product.image} alt="product image" className="product-card-image" />
                 <div className="product-card-content">
                     <h3 className="product-card-name">{product.name}</h3>
                     <p className="product-card-price">{product.price}</p>
